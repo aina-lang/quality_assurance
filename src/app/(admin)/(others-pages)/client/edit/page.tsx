d40ff0d8ece35bad514513576eea84ac7add8fba
aina-lang <merciaaina@gmail.com>
2025-10-29 16:18:56 +0300

    webpack stripe import fixed

---
+++ src/app/(admin)/(others-pages)/client/edit/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+export default function AddClientPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Ajouter un client</h1>
+      {/* ton formulaire ici */}
+    </div>
+  );
+}
